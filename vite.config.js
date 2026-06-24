d0dfb0c8d9ac81b543f97217407ad777fa24dc76
GARVITAMITBHUTANI <garvitamitbhutani@gmail.com>
2026-06-24 18:06:22 +0530

    Update vite.config.js to build multiple HTML pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        toneva: resolve(__dirname, 'toneva.html'),
+        trichovva: resolve(__dirname, 'trichovva.html'),
+      },
+    },
+  },
 })
